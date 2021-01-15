8c50ac00f072cf3299dd2696dc4d90e73bd756d9
Asim Shrestha <asim.shrestha@hotmail.com>
2021-01-15 11:53:40 -0800

    feat: add base package stuff

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name="example-pkg-asim-shrestha", # Replace with your own username
+    version="0.0.1",
+    author="Asim Shrestha",
+    author_email="asim.shrestha@hotmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=required,
+)
