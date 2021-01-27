427c97c0135b5bacb21601c05ac1c9c594424f5d
Asim Shrestha <ashresth@sfu.ca>
2021-01-27 15:14:58 -0800

    refactor: bump version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-VERSION = '0.0.5'
+VERSION = '0.0.6'
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 with open('requirements.txt') as f:
