dabe60cac9bae0b045dd3f3aee24277d6660911c
Adam <cadamrun@gmail.com>
2021-03-01 16:54:09 -0800

    :feat: adding color palette

--- rt_utils/utils.py
+++ rt_utils/utils.py
@@ -2,11 +2,30 @@ from random import randrange
 from pydicom.uid import PYDICOM_ROOT_UID
 from dataclasses import dataclass
 
-class SOPClassUID():
-    RTSTRUCT_IMPLEMENTATION_CLASS = PYDICOM_ROOT_UID # TODO find out if this is ok
+COLOR_PALLET = [
+    [255, 0, 0],        # Red
+    [0, 255, 0],        # Lime
+    [0, 0, 255],        # Blue
+    [255, 255, 0],      # Yellow
+    [0, 255, 255],      # Cyan
+    [255, 0, 255],      # Magenta
+    [192, 192, 192],    # Silver
+    [128, 128, 128],    # Gray
+    [128, 0, 0],        # Maroon
+    [128, 128, 0],      # Olive
+    [0, 128, 0],        # Green
+    [128, 0, 128],      # Purple
+    [0, 128, 128],      # Teal
+    [0, 0, 128]         # Navy
+]
+
+
+class SOPClassUID:
+    RTSTRUCT_IMPLEMENTATION_CLASS = PYDICOM_ROOT_UID  # TODO find out if this is ok
     CT_IMAGE_STORAGE = '1.2.840.10008.5.1.4.1.1.2'
     DETACHED_STUDY_MANAGEMENT = '1.2.840.10008.3.1.2.3.1'
     RTSTRUCT = '1.2.840.10008.5.1.4.1.1.481.3'
+
 
 @dataclass
 class ROIData:
@@ -23,12 +42,8 @@ class ROIData:
         self.add_default_values()
 
     def add_default_values(self):
-        if self.color == None:
-            self.color = self.get_random_colour()
+        if self.color is None:
+            self.color = COLOR_PALLET[(self.number - 1) % len(COLOR_PALLET)]
 
-        if self.name == None:
+        if self.name is None:
             self.name = f"ROI-{self.number}"
-    
-    def get_random_colour(self):
-        max = 256
-        return [randrange(max), randrange(max), randrange(max)]
---
+++ tests/test_utils.py
@@ -0,0 +1,5 @@
+from rt_utils.utils import ROIData
+
+
+def test_roi_color_data():
+    pass
