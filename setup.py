47c649a428a4258e1e635917c28737378085dae6
Asim Shrestha <ashresth@sfu.ca>
2022-01-07 18:59:13 -0800

    feat: update version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-VERSION = '1.1.7'
+VERSION = '1.1.8'
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 with open('requirements.txt') as f:
