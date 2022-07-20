29e6afcadff758e18b2d264d4118f5825cd57f83
Asim Shrestha <ashresth@sfu.ca>
2022-07-20 00:57:13 -0700

    feat: update pydicom requirement

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-VERSION = "1.2.1"
+VERSION = "1.2.2"
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 with open("requirements.txt") as f:
