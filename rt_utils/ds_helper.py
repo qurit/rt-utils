516de85d15268f4bfdd2f4b6c5f3ad294c6720cc
Asim Shrestha <ashresth@sfu.ca>
2023-01-05 19:52:49 -0800

    🔢 Use existing frame of reference uid if available

--- rt_utils/ds_helper.py
+++ rt_utils/ds_helper.py
@@ -98,12 +98,8 @@ def add_patient_information(ds: FileDataset, series_data):
 
 def add_refd_frame_of_ref_sequence(ds: FileDataset, series_data):
     refd_frame_of_ref = Dataset()
-    refd_frame_of_ref.FrameOfReferenceUID = (
-        generate_uid()
-    )  # TODO Find out if random generation is ok
-    refd_frame_of_ref.RTReferencedStudySequence = create_frame_of_ref_study_sequence(
-        series_data
-    )
+    refd_frame_of_ref.FrameOfReferenceUID =  getattr(series_data[0], 'FrameOfReferenceUID', generate_uid())
+    refd_frame_of_ref.RTReferencedStudySequence = create_frame_of_ref_study_sequence(series_data)
 
     # Add to sequence
     ds.ReferencedFrameOfReferenceSequence = Sequence()
