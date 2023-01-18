fb2a33e3a3c94762ca95450b00fd3b374d28f5a3
zhack47 <mesbah.zacharia@sfr.fr>
2023-01-18 12:20:52 +0100

    Fix missing import

--- rt_utils/rtstruct_builder.py
+++ rt_utils/rtstruct_builder.py
@@ -2,6 +2,8 @@ from typing import List
 from pydicom.dataset import Dataset
 from pydicom.filereader import dcmread
 
+import warnings
+
 from rt_utils.utils import SOPClassUID
 from . import ds_helper, image_helper
 from .rtstruct import RTStruct
